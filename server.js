c3b665843d8948264ff25ea0546417d2711ae12d
tompedro <55707296+tompedro@users.noreply.github.com>
2019-10-29 19:50:10 +0100

    versione 2.0

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 let express = require("express");
 let myParser = require("body-parser");
+let mysql = require('mysql');
 let app = express();
 
 let p = [];
+let con = mysql.createConnection({
+    host:"localhost",
+    user:"admin",
+    password:"admin",
+    database:"db"
+});
+
+function parse(json,n,int){
+    let _j = JSON.stringify(json);
+    _j = _j.replace("{","");
+    _j = _j.replace("}","");
+    return( _j.split(",")[n].split(":")[int]);
+}
 
 app.use(myParser.urlencoded({extended : true}));
 app.post("/", function(request, response) {
@@ -14,8 +28,24 @@ app.post("/", function(request, response) {
         p.push(request.body);
         console.log(request.body);
     }
-    
 });
+
+app.post("/login",function(request,response){
+    response.setHeader("Access-Control-Allow-Origin","*");
+    con.connect(function(err){
+        if(err) throw err;
+        console.log("connected to db");
+        let str;
+        str = parse(request.body,0,0) + "," +parse(request.body,0,1) + ","+parse(request.body,1,0) + ","+"true";
+        console.log(str);
+        let sql = "INSERT INTO Accounts(Users,Password,Ip,Connected) VALUES("+str+")";
+        con.query(sql,function(err){
+            if(err) throw err;
+            console.log("done");
+        });
+    });
+});
+
 app.get("/", function(request, response) {
     response.setHeader("Access-Control-Allow-Origin","*");
     response.send(JSON.stringify(p));
