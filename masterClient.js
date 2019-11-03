7693c3f8199335069d499b929d0044d53b25c67f
tompedro <55707296+tompedro@users.noreply.github.com>
2019-11-03 15:07:55 +0100

    Delete masterClient.js

--- masterClient.js
+++
@@ -1,9 +0,0 @@
-const superagent = require('superagent');
-
-superagent
-    .post('http://127.0.0.1:8080/master')
-    .then(res => {
-        console.log('yay got ' + JSON.stringify(res.body));
-    });
-
-    
